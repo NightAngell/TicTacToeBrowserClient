e48a065797efa122c40cdaa0025aed41a5a2ee7e
NightAngell <sobta24@wp.pl>
2018-11-26 14:46:31 +0100

    Created main menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IconPadlockComponent } from './shared/icons/icon-padlock/icon-padlock.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IconPadlockComponent
+    IconPadlockComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
