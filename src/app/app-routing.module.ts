19e8f8a1cd07120c89d2f1e0befc79fe6cb6e584
NightAngell <sobta24@wp.pl>
2018-11-26 15:05:34 +0100

    Added some style

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
+import { TicTacToeSingleplayerComponent } from './tic-tac-toe-singleplayer/tic-tac-toe-singleplayer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: MenuComponent },
+  { path: "ticTacToe", component: TicTacToeSingleplayerComponent },
+  // { path: ""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
